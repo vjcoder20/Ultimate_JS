f348157b421dfc4e564ec4c151f0cbc7b3371999
vjcoder20 <jain.vaibhav020@gmail.com>
2024-04-29 00:23:13 +0530

    some update in variables file

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,14 +1,14 @@
-const accountId = 144553
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
+const accountId = 11793
+let accountEmail = "vaibhav@google.com"
+var accountPassword = "9801"
 accountCity = "Jaipur"  //we can define the variable without any keyword but not a good practice
 let accountState;    //If we are not initializing any value then the result would be undefined
 
-// accountId = 2 // not allowed because we can't change the values of constant
+// accountId = 32325 // not allowed because we can't change the values of constant
 
 
-accountEmail = "hc@hc.com"
-accountPassword = "21212121"
+accountEmail = "vj@vj.com"
+accountPassword = "6377356958"
 accountCity = "Bengaluru"
 
 console.log(accountId);
@@ -19,4 +19,5 @@ because of issue in block scope and functional scope
 */
 
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) 
+// It will produce result in table format for all the variables
